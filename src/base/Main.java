a1304bd94444a270b624333fd8097475209a770a
Daniel Migales <daniel.migales.puertas@gmail.com>
2020-03-22 16:58:16 +0100

    v.1.1

---
+++ src/base/Main.java
@@ -0,0 +1,41 @@
+package base;
+
+import java.io.IOException;
+import java.util.Scanner;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+/**
+ * @author Daniel Migales
+ */
+public class Main {
+    
+    public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException {
+        
+        Gestor gestion = new Gestor();
+        Scanner teclado = new Scanner(System.in);
+        int seleccion;
+        boolean salir = true;
+        
+        do {            
+            System.out.println("*********MENU PRINCIPAL*********\n");
+            System.out.println("1. Realizar scrapping de peliculas e imprimir el resultado en pantalla.)");
+            System.out.println("2. Salir del programa.");
+            System.out.print("\nIntroduzca su seleccion : ");
+             seleccion = teclado.nextInt();
+            
+            switch (seleccion) {
+                case 1:
+                    System.out.println("\nLISTADO DE PELICULAS ENCONTRADAS: ");
+                    gestion.scrappingHtml();
+                    System.out.println("\nSE HA GENERADO UN ARCHIVO XML CON EL RESULTADO.");
+                    gestion.crearXML();
+                    break;
+                case 2:  
+                    salir = false;
+                    break;
+            }
+        } while (salir);
+        
+    }
+}
